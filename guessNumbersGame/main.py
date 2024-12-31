bb6b4ec7ade396cce8292106b5636b9330c56307
Jonathan <jonathancornejox@gmail.com>
2024-12-31 14:00:13 -0800

    fixed exception block

--- guessNumbersGame/main.py
+++ guessNumbersGame/main.py
@@ -9,13 +9,13 @@ print(f"Guess the number in the range from {lower_num} to {higher_num}.")
 while True:
     try:
         user_guess: int = int(input("Guess: "))
-    except ValueError as e:
-        print("Enter a valid number: {e}")
+    except ValueError as err:
+        print(f"Enter a valid number: {err}")
         continue
     if user_guess > random_number:
         print("The number is lower")
     elif user_guess < random_number:
         print("The number is higher")
     else:
-        print("You guess the number!")
+        print(f"You guessed the number: it is {random_number}!")
         break
