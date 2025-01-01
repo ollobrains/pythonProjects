edf126a6822aeea76c250300fecd2c37c40dff14
Jonathan <jonathancornejox@gmail.com>
2024-12-31 19:08:44 -0800

    covered edge case of long string guess user inputs

--- hangman/main.py
+++ hangman/main.py
@@ -28,19 +28,22 @@ def run_game():
             break
         guess: str = input('Enter a letter: ')
 
+        if len(guess) > 1:
+            for char in guess:
+                if char not in word:
+                    tries -= 1
+                    if tries == 0:
+                        print(f'You ran out of tries... ({tries} tries remaining)')
+                        break
+        elif guess not in word:
+            tries -= 1
+            print(f'Sorry, that was wrong... ({tries} tries remaining)')
+
         if guess in guessed:
             print(f'You already used: "{guess}". Please try another letter')
             continue
 
         guessed += guess
-
-        if guess not in word:
-            tries -= 1
-            print(f'Sorry, that was wrong... ({tries} tries remaining)')
-
-            if tries == 0:
-                print('No more tries remaining...')
-                break
 
 
 if __name__ == '__main__':
