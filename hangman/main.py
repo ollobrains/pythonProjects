ffe94e17a9ce9afb412c0dffa96501a2424ecf79
ollobrains <et_pj84@hotmail.com>
2025-01-01 10:38:15 +1000

    Update main.py
    Signed-off-by: ollobrains <et_pj84@hotmail.com>

--- hangman/main.py
+++ hangman/main.py
@@ -1,47 +1,70 @@
 import random
 
-
 def run_game():
-    word: str = random.choice(['apple', 'secret', 'banana'])
+    # You can keep adding more words to the list if you like
+    word_list = [
+        'apple', 'secret', 'banana', 'python', 'keyboard',
+        'mountain', 'hangman', 'jupyter', 'developer'
+    ]
 
-    username: str = input('What is your name? >>')
-    print(f'Welcome to hangman, {username}')
+    word: str = random.choice(word_list)
 
-    guessed: str = ''
+    username: str = input('What is your name? >> ')
+    print(f'Welcome to hangman, {username}!\n")
 
-    tries: int = 3
+    guessed_letters: str = ''
+    tries: int = 5
 
     while tries > 0:
         blanks: int = 0
 
+        # Show the current state of the guessed word
         print('Word: ', end='')
         for char in word:
-            if char in guessed:
+            if char in guessed_letters:
                 print(char, end='')
             else:
                 print('_', end='')
                 blanks += 1
-        print()  # adds a blank line
+        print()  # Move to a new line
 
+        # If no blanks, the user has guessed all letters
         if blanks == 0:
-            print('You guessed the word.')
+            print(f"Congratulations! You correctly guessed '{word}'!")
             break
-        guess: str = input('Enter a letter: ')
 
-        if guess in guessed:
-            print(f'You already used: "{guess}". Please try another letter')
+        guess: str = input("Enter a single letter: ").strip().lower()
+
+        # Validate input: must be a single alpha character
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please enter exactly one letter (A-Z).")
             continue
 
-        guessed += guess
+        # Check if letter was already guessed
+        if guess in guessed_letters:
+            print(f'You already used "{guess}". Try another letter.')
+            continue
 
+        # Add the guess to the set of guessed letters
+        guessed_letters += guess
+
+        # If guess not in word, decrement tries
         if guess not in word:
             tries -= 1
-            print(f'Sorry, that was wrong... ({tries} tries remaining)')
-
-            if tries == 0:
-                print('No more tries remaining...')
+            if tries > 0:
+                print(f"Sorry, '{guess}' is not in the word. ({tries} tries remaining)\n")
+            else:
+                print("No more tries remaining...")
+                print(f"The word was '{word}'. Better luck next time!\n")
                 break
 
+def main():
+    while True:
+        run_game()
+        replay = input("Would you like to play again? (y/n): ").strip().lower()
+        if replay != 'y':
+            print("Thanks for playing! Goodbye.")
+            break
 
 if __name__ == '__main__':
-    run_game()
+    main()
